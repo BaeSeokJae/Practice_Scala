34d0f98c0fe98ca6eebb0864cdd7da294f3c925c
BaeSeokJae <bsjp9400@gmail.com>
2021-05-13 18:38:38 +0900

    completed ~problem7

--- src/main/scala/Euler_Project.scala
+++ src/main/scala/Euler_Project.scala
@@ -2,10 +2,12 @@ import Problem_List._
 
 object Euler_Project {
   def main(args: Array[String]): Unit = {
-    println("problem1: ",problem1(1000));
-    println("problem2: ",problem2(4000000));
-    println("problem3: ",problem3(600851475143L, 2L));
-    println("problem4: ",problem4());
-    println("problem5: ",problem5(20))
+    println("problem1: ", problem1(1000));
+    println("problem2: ", problem2(4000000));
+    println("problem3: ", problem3(600851475143L, 2L));
+    println("problem4: ", problem4());
+    println("problem5: ", problem5(20))
+    println("problem6: ", problem6())
+    println("problem7: ", problem7())
   }
 }
--- src/main/scala/Problem_List.scala
+++ src/main/scala/Problem_List.scala
@@ -3,7 +3,7 @@ import scala.util.control.Breaks._
 object Problem_List {
   def problem1(n: Number): Int = {
     var sum = 0;
-    for ( i <- 0 until n.intValue()) {
+    for (i <- 0 until n.intValue()) {
       if (i % 3 == 0 || i % 5 == 0) {
         sum += i;
       }
@@ -36,13 +36,13 @@ object Problem_List {
    * 따라서 재귀함수를 사용하여 인자값을 소인수로 나눈 값을 넘김
    * 최종적으로 인자값이 1이 될때의 해당 소인수값을 리턴하게 됌.
    */
-  def problem3(num: Long, i : Long) :Long= {
-    if(num == 1)
-      i-1
-    else if(num%i==0)
-      problem3(num/i , i+1)
+  def problem3(num: Long, i: Long): Long = {
+    if (num == 1)
+      i - 1
+    else if (num % i == 0)
+      problem3(num / i, i + 1)
     else
-      problem3(num, i+1)
+      problem3(num, i + 1)
   }
 
   def problem4(): Int = {
@@ -50,25 +50,58 @@ object Problem_List {
     var reverseSum = ""
     var maxSum = 0;
     for (i <- 100 until 999; j <- 100 until 999) {
-        sum = i * j;
-        reverseSum = sum.toString.reverse
-        if (sum.toString == reverseSum && sum > maxSum) {
-          maxSum = sum;
-        }
+      sum = i * j;
+      reverseSum = sum.toString.reverse
+      if (sum.toString == reverseSum && sum > maxSum) {
+        maxSum = sum;
       }
-  return maxSum;
+    }
+    return maxSum;
   }
 
   // TODO: 리팩토링 고려해보기
   def problem5(n: Long): Long = {
-    def max(a :Long , b :Long): Long = {
-      if(b == 0) a
-      else max(b , a%b)
+    def max(a: Long, b: Long): Long = {
+      if (b == 0) a
+      else max(b, a % b)
     }
+
     var b: Long = 1
-    for( i <-2L to n){
+    for (i <- 2L to n) {
       b = b * i / max(b, i)
     }
     b
+  }
+
+  def problem6() = {
+    var sumOfSquares: BigInt = 0;
+    var sum = 0;
+    for (i <- 1 until 101) {
+      sumOfSquares = sumOfSquares + BigInt(i).pow(2);
+      sum += i;
+    }
+    var squaredSum = BigInt(sum).pow(2)
+    squaredSum - sumOfSquares
+  }
+
+  def problem7() = {
+    var cnt = 0;
+    var decimal = 2;
+    var result = 0;
+    var loop = false;
+    def isPrime(num:Int):Boolean =
+      (num > 1) && !(2 to scala.math.sqrt(num).toInt).exists(x => num % x == 0)
+
+    while (!loop) {
+      if (isPrime(decimal)) {
+        cnt += 1;
+        if (cnt == 10001) {
+          result += decimal;
+          loop = true
+        }
+      }
+      decimal += 1;
+    }
+    result;
   }
 }
